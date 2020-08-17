331d1ad173ba8d6c717e0ca95ca636e774c8307a
WalkerMozes <mozeswalker@outlook.com>
2020-08-17 10:02:03 +0100

    began implementing search field and detect button

--- smartbrain-app/src/App.js
+++ smartbrain-app/src/App.js
@@ -119,6 +119,24 @@ const particlesOptions = {
 };
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value)
+  }
+
+  onSubmit = () => {
+    console.log('click');
+  }
+
+
+
+
   render() {
     return (
       <div className="App">
@@ -128,7 +146,9 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onSubmit={this.onSubmit}/>
         {/* <FaceRecognition /> */}
       </div>
     );

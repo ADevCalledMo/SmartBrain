55a22da7dd4f858ea53f5efb423b88e1c588b2a1
WalkerMozes <mozeswalker@outlook.com>
2020-09-14 14:51:54 +0100

    added route paths to the sign in form , also added Register page

--- smartbrain-app/src/App.js
+++ smartbrain-app/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import Particles from 'react-particles-js';
-import Clarifai from 'clarifai'
+import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 import Navigation from "./Components/Navigation/Navigation";
-import SignIn from "./Components/Signin/SignIn"
+import SignIn from "./Components/Signin/SignIn";
+import Register from "./Components/Register/Register";
 import FaceRecognition from "./Components/FaceRecognition/FaceRecognition";
 import Logo from "./Components/Logo/Logo";
 import ImageLinkForm from "./Components/ImageLinkform/ImageLinkForm";
@@ -10,9 +11,8 @@ import Rank from "./Components/Rank/Rank";
 import "./App.css";
 
 const app = new Clarifai.App({
-  apiKey: '82346885f77f42b1ab6ccf712a0ef66b'
- });
-
+  apiKey: "82346885f77f42b1ab6ccf712a0ef66b"
+});
 
 const particlesOptions = {
   particles: {
@@ -129,63 +129,73 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      input: '',
-      imageUrl: '', 
-      box: {}
-    }
+      input: "",
+      imageUrl: "",
+      box: {},
+      route: "signin"
+    };
   }
 
-  calculateFaceLocation = (data) => {
-    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
-    const image = document.getElementById('inputimage');
-    const width = Number(image.width)
-    const height = Number(image.height)
+  calculateFaceLocation = data => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputimage");
+    const width = Number(image.width);
+    const height = Number(image.height);
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
-  }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height
+    };
+  };
 
-  displayFaceBox = (box) => {
-    console.log(box)
-    this.setState({box})
-  }
+  displayFaceBox = box => {
+    console.log(box);
+    this.setState({ box });
+  };
 
-  onInputChange = (event) => {
-    this.setState({input: event.target.value})
-
-  }
+  onInputChange = event => {
+    this.setState({ input: event.target.value });
+  };
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
+    this.setState({ imageUrl: this.state.input });
     app.models
-    .predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input)
-      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-      .catch(err => console.log(err))
-  
-  }
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then(response =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch(err => console.log(err));
+  };
 
-
-
+  onRouteChange = route => {
+    this.setState({ route: route });
+  };
 
   render() {
     return (
       <div className="App">
-        <Particles className='particles'
-          params={particlesOptions}
-        />
-        <Navigation />
-        <SignIn />
-        <Logo />
-        <Rank />
-        <ImageLinkForm 
-          onInputChange={this.onInputChange} 
-          onButtonSubmit={this.onButtonSubmit}/>
-       <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <Particles className="particles" params={particlesOptions} />
+        <Navigation onRouteChange={this.onRouteChange} />
+        {this.state.route === "home" ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              box={this.state.box}
+              imageUrl={this.state.imageUrl}
+            />
+          </div>
+        ) : this.state.route === "signin" ? (
+          <SignIn onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
--- smartbrain-app/src/Components/Register/Register.js
+++ smartbrain-app/src/Components/Register/Register.js
@@ -1,12 +1,16 @@
 import React from "react";
 
-const SignIn = ({ onRouteChange }) => {
+const Register = ({ onRouteChange }) => {
   return (
     <article className="mw6  center bg-white br3 pa3 pa4-ns mv3 ba  shadow-4 b--black-10">
 <main className="pa4 black-80">
   <form className="measure">
     <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-      <legend className="f2 fw6 ph0 mh0 center">Sign In</legend>
+      <legend className="f2 fw6 ph0 mh0 center">Register</legend>
+      <div className="mt3">
+        <label className="db fw6 lh-copy f4" for="name">Name</label>
+        <input className="pa3 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="name" name="name"  id="name" />
+      </div>
       <div className="mt3">
         <label className="db fw6 lh-copy f4" for="email-address">Email</label>
         <input className="pa3 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
@@ -15,19 +19,14 @@ const SignIn = ({ onRouteChange }) => {
         <label className="db fw6 lh-copy f4" for="password">Password</label>
         <input className="b pa3 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
       </div>
-
     </fieldset>
     <div className="">
       <input 
-        onClick={() => onRouteChange('home')}
+        onClick={() => onRouteChange('register')}
         className="b ph4 pv2 input-reset ba b--black bg-transparent grow pointer f3 dib"
         type="submit"
-        value="Sign in"
+        value="Register"
       />
-    </div>
-    <div className="lh-copy mt3">
-      <p onClick={() => onRouteChange('register')} className="f4 link dim black db pointer">Register</p>
-
     </div>
   </form>
 </main>
@@ -35,4 +34,4 @@ const SignIn = ({ onRouteChange }) => {
   );
 };
 
-export default SignIn;
+export default Register;
